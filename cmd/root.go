f5433e1b5d32fc122c0aa0bd86b31e31a598fc86
Ramax Lo <ramaxlo@rldevs.net>
2026-03-18 21:03:37 +0800

    Print elapsed time only for 'yocto' subcommands
    Move 'PersistentPreRun' & 'PersistentPostRun' settings from rootCmd to
    yoctoCmd so that only 'yocto' subcommands print elapsed time.
    
    Signed-off-by: Ramax Lo <ramaxlo@rldevs.net>

--- cmd/root.go
+++ cmd/root.go
@@ -3,24 +3,16 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/spf13/cobra"
 )
 
 var cfgFile string
 var dryRun bool
-var startTime time.Time
 
 var rootCmd = &cobra.Command{
 	Use:   "ak",
 	Short: "Automated Yocto build environment manager",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		startTime = time.Now()
-	},
-	PersistentPostRun: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Elapsed time: %s\n", time.Since(startTime).Round(time.Second))
-	},
 }
 
 func init() {
--- cmd/yocto.go
+++ cmd/yocto.go
@@ -1,10 +1,23 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var startTime time.Time
 
 var yoctoCmd = &cobra.Command{
 	Use:   "yocto",
 	Short: "Yocto build environment commands",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		startTime = time.Now()
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Elapsed time: %s\n", time.Since(startTime).Round(time.Second))
+	},
 }
 
 func init() {
